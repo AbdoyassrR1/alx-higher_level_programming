58dc2d3d3ebe1fe9f1e14ba1e97c412e5ec94361
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-09 00:35:54 +0200

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number == 0:
-        return 0
+        print(0)
     elif number < 0:
         number *= -1
-        return number % 10
+        print(number % 10)
     else:
-        return number % 10
+        print(number % 10)

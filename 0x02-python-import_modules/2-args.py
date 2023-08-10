c908e1ebc4468e5b20c2852a931becc3a846f0ec
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-11 00:59:46 +0200

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    args = sys.argv
+    if len(args) == 1:
+        print("0 arguments.")
+    else:
+        i = 1
+        print("{} arguments:".format(len(args) - 1))
+        while i  < len(args):
+            print("{}: {}".format(i , args[i]))
+            i += 1

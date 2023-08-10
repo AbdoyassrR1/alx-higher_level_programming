58def67fa39c98950fa193d8f6559f1a2e5ba5c5
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-11 00:32:36 +0200

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
 
-c = add(a, b)
+    a = 1
+    b = 2
+
+    c = add(a, b)
 
 
-print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, c))

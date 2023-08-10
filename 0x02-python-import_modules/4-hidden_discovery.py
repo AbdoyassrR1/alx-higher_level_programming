2de0673747f71804729610c911f974012ab7dd31
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-11 01:44:13 +0200

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hddien_4
 
-    name = dir(hddien_4)
+if __name__ == "__main__":
+
+    import hidden_4
+
+    name = dir(hidden_4)
 
     for n in name:
-        if n[0:2] != "__":
-            print(name)
+        if n[:2] != "__":
+            print(n)

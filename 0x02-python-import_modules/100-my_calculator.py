7f323baea84b1edddf774d3dbc88789090535790
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-11 17:51:10 +0200

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,19 +16,19 @@ if __name__ == "__main__":
     elif len_of_args == 3:
         if args[2] == "+":
             print("{} + {} = {}".format(int(args[1]), int(args[3]),
-                                        int(args[1]) + int(args[3])))
+                                        add(int(args[1]), int(args[3]))))
 
         elif args[2] == "-":
             print("{} + {} = {}".format(int(args[1]), int(args[3]),
-                                        int(args[1]) - int(args[3])))
+                                        sub(int(args[1]), int(args[3]))))
 
         elif args[2] == "*":
             print("{} + {} = {}".format(int(args[1]), int(args[3]),
-                                        int(args[1]) * int(args[3])))
+                                        mul(int(args[1]), int(args[3]))))
 
         elif args[2] == "/":
             print("{} + {} = {}".format(int(args[1]), int(args[3]),
-                                        int(args[1]) / int(args[3])))
+                                        div(int(args[1]), int(args[3]))))
 
         else:
             print("Unknown operator. Available operators: +, -, * and /")

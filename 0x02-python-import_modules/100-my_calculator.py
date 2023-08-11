23131f6ee022fb6f2b51b6a3608ff0fbf60b9114
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-11 17:36:56 +0200

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+from calculator_1 import add, sub, mul, div
+import sys
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
 
     i = 1
     args = sys.argv

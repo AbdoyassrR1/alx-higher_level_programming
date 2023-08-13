46d9625743c4b8fe583c9764d0d31dedb66c8fbe
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-13 03:08:57 +0200

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if my_list is False:
+        return None
+
+    else:
+        my_list.sort()
+        max_value = my_list[-1]
+        return max_value

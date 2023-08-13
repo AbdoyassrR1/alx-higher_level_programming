a3f396a0b6fc10fd5c3f70a1a73bac00b70445c1
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-13 17:04:40 +0200

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,32 +10,7 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *curr = *head;
-	int arr[1024];
-	int len = 0;
-	int i;
-
-
-	if (*head == NULL || head != NULL)
-	{
-		return (1);
-	}
-
-	while (curr != NULL)
-	{
-		arr[len] = curr->n;
-		curr = curr->next;
-		len++;
-	}
-
-	for (i = 0; i < len / 2; i++)
-	{
-		if (arr[i] != arr[len - i - 1])
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	listint_t *list_a = *head;
+	listint_t *list_b = *head;
 
 }

df8e4f0e6111736abe446302dbdc18ae230be65f
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-17 03:55:38 +0200

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,13 +2,13 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
         nums = []
         for key in a_dictionary:
-          nums.append(a_dictionary[key])
-        
+            nums.append(a_dictionary[key])
+
         for key, val in a_dictionary.items():
             if val == max(nums):
                 best = key

30b69410d1daa669e7112a2c627d54afaaaf852e
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-08 21:46:24 +0300

    addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
 """
-this module contain functoin that adds two numbers
+This is the 0-add_integer module.
 """
 
 
 def add_integer(a, b=98):
-    """functoin to add numbers"""
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
+    """
+       Return an integer: sum of 2 numbers
+    """
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    return (a + b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer or float")
+    return a + b

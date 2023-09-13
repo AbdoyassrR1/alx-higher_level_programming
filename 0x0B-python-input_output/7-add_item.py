449fff57e3f44fed9b41c5b14bd669ba2f61aacc
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-13 05:06:37 +0300

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-
-
+"""module doc"""
 import sys
 import os
 
+
 if __name__ == "__main__":
-    
+
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file

333e7ae1f9a4f18553363100a600458f68e5bb6a
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-19 05:21:56 +0300

    add update function

--- 0x0C-python-almost_a_circle/modles/square.py
+++ 0x0C-python-almost_a_circle/modles/square.py
@@ -35,3 +35,23 @@ class Square(Rectangle):
         """returns the dictionary representation of a Square"""
 
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
+    
+    def update(self, *args, **kwargs):
+        """that assigns attr1st argument should be the id attribute
+            2nd argument should be the size attribute
+            3rd argument should be the x attribute
+            4th argument should be the y attribute
+        """
+        if args and len(args) != 0:
+            for arg in args:
+                if len(args) >= 1:
+                    self.id = args[0]
+                if len(args) >= 2:
+                    self.size = args[1]
+                if len(args) >= 3:
+                    self.x = args[2]
+                if len(args) >= 3:
+                    self.y = args[3]
+            else:
+                for key, value in kwargs.items():
+                    return (key, value)

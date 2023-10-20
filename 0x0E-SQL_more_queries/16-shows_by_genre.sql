285fc5aac580769a3842bf9337cd96ba6591ad93
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-20 05:44:16 +0300

    List shows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-SELECT title, name
-FROM tv_shows
-LEFT  JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_shows_genres.genre_id
-ORDER BY title ASC, name ASC;
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;

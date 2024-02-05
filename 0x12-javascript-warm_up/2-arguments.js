d02218f25dc1693d885a60b4ae784b3c32a44f55
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-02-05 04:57:40 +0200

    warm up js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints a msg depending of the number of args passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

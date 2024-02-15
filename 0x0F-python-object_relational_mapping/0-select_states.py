e5a281eba3ea3e8851bf0beff4144c7a001de363
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-02-16 00:36:18 +0200

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Get all states
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    args = sys.argv
+    connection = MySQLdb.connect(
+        user=args[1],
+        password=args[2],
+        database=args[3]
+        )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    for state in cursor.fetchall():
+        print(state)

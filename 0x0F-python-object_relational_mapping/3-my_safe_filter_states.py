9986fecd7b4d55455dd10576bb9d957957fa58fa
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-02-16 23:19:10 +0200

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+write a script safe form SQL injection
+usage file name\
+    <database user name>\
+    <database password>\
+    <database name>\
+    <name searched>
+"""
+import sys
+import MySQLdb
+
+connection = MySQLdb.connect(
+    user=sys.argv[1],
+    password=sys.argv[2],
+    database=sys.argv[3]
+)
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM states")
+for state in cursor.fetchall():
+    if state[1] == sys.argv[4]:
+        print(state)
